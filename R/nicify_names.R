fd0c9fc3c09acee1005c5a44c26c75ed7922c2bd
Albert Z Wang <albert.zhao.wang@gmail.com>
2016-06-24 12:01:25 -0400

    additional name cleaning

--- R/nicify_names.R
+++ R/nicify_names.R
@@ -6,6 +6,8 @@
 nicify_names <- function(df, lowercase = TRUE, abbrev = FALSE){
   names(df) <- names(df) %>%
     do_if(lowercase, tolower) %>%
+    str_replace_all("[^A-Za-z0-9]+", "_") %>%
+    str_replace_all("^_+|_+$", "") %>%
     make.names(unique = TRUE, allow_ = TRUE)
   df
 }
