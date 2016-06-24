cd8b7f1b5d065d865e1658a39ad135e6fc1c9535
Albert Z Wang <albert.zhao.wang@gmail.com>
2016-06-24 11:58:35 -0400

    update

--- R/nicify_names.R
+++ R/nicify_names.R
@@ -4,5 +4,14 @@
 #' @importFrom dplyr %>%
 #' @export
 nicify_names <- function(df, lowercase = TRUE, abbrev = FALSE){
+  names(df) <- names(df) %>%
+    do_if(lowercase, tolower) %>%
+    make.names(unique = TRUE, allow_ = TRUE)
+  df
+}
 
+#' @importFrom purrr invoke
+do_if <- function(.x, .p, .f){
+  my_f <- if (isTRUE(.p)) .f else identity
+  invoke(my_f, list(.x))
 }
