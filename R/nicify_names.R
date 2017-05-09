b8ed8830a31ec611c88b60b5b17b35aa6e49a675
Albert Z Wang <albert.zhao.wang@gmail.com>
2017-05-09 10:25:12 -0400

    Update nicify_names
    Add camelCase detection

--- R/nicify_names.R
+++ R/nicify_names.R
@@ -3,10 +3,10 @@
 #' @importFrom stringr str_replace_all str_replace
 #' @importFrom dplyr %>%
 #' @param df Dataframe
-#' @param lowercase Whether to lowercase names or not
-#' @param abbrev Unused.
+#' @param style Naming style convention.  Currently only 'snake_case' is supported.
+#' @param abbrev Logical. Whether to use common abbreviations such as 'pct' for '\%'
 #' @export
-nicify_names <- function(df, lowercase = TRUE, abbrev = FALSE){
+nicify_names <- function(df, style = c("snake_case"), abbrev = FALSE){
   newnames <- names(df)
   if (abbrev){
     newnames <- newnames %>%
@@ -14,8 +14,8 @@ nicify_names <- function(df, lowercase = TRUE, abbrev = FALSE){
   }
   newnames <- newnames %>%
     do_if(lowercase, tolower) %>%
-    str_replace_all("[^A-Za-z0-9]+", "_") %>%
-    str_replace_all("^_+|_+$", "") %>%
+    sep_words_all %>%
+    words_to_snake %>%
     make.names(unique = TRUE, allow_ = TRUE)
   names(df) <- newnames
   df
@@ -25,4 +25,38 @@ nicify_names <- function(df, lowercase = TRUE, abbrev = FALSE){
 do_if <- function(.x, .p, .f){
   my_f <- if (isTRUE(.p)) .f else identity
   invoke(my_f, list(.x))
+}
+
+#' @importFrom stringi stri_replace_all_regex stri_split_regex
+#' @importFrom purrr map flatten_chr
+sep_words_camelcase <- function(x){
+  do_split <- function(.x){
+    stri_split_regex(.x, "(?<=[A-Za-z])(?=[A-Z][a-z])")
+  }
+  if (is.list(x)) {
+    return(map(x, ~ flatten_chr(do_split(.x))))
+  }
+  return(do_split(x))
+}
+
+sep_words_nonalphanum <- function(x){
+  do_split <- function(.x){
+    stri_split_regex(.x, "[^A-Za-z0-9]+")
+  }
+  if (is.list(x)) {
+    return(map(x, ~ flatten_chr(do_split(.x))))
+  }
+  return(do_split(x))
+}
+
+sep_words_all <- function(x){
+  x %>% sep_words_nonalphanum %>%
+    sep_words_camelcase %>%
+    map(~ discard(.x, ~ nchar(.x) < 1))
+}
+
+#' @importFrom stringr str_c
+words_to_snake <- function(x){
+  x %>% map(tolower) %>%
+    map(~ str_c(.x, collapse="_"))
 }
